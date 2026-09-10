e0b76d40f961ed9b1a22d31f4b8972535c1d67aa
Fiona Friberg <fionafriberg@outlook.com>
2026-09-10 10:39:47 +0200

    initial setup for addTodoView

---
+++ src/views/AddTodoView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import addTodo from '../components/AddTodo.vue'
+</script>
+
+<template>
+    <main>
+        <addTodo/>
+        
+        <RouterLink to="/">
+        Todo
+        </RouterLink>
+    </main>
+</template>
