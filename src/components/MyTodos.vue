5d1c08594650cc65f37ca7e567f4ef3fc0a60426
Fiona Friberg <fionafriberg@outlook.com>
2026-09-12 14:06:00 +0200

    show todos as cards

--- src/components/MyTodos.vue
+++ src/components/MyTodos.vue
@@ -12,9 +12,45 @@ const todos = ref(
     <ul>
         <li
             v-for="todo in todos"
-            :key="todos"
+            :key="todo.title"
         >
-            {{ todos }}
+
+            <h4>{{ todo.taskList }}</h4>
+
+            <h2>{{ todo.title }}</h2>
+
+            <h3>{{ todo.time }} minutes</h3>
+
+            <p>Deadline: {{ todo.deadline }}</p>
+
+            <p>{{ todo.description }}</p>
+
+            <p>Repeat this task? {{ todo.repeat }}</p>
+            <p v-if="todo.repeat === 'yes'">
+                Schedule: {{ todo.repeatSchedule }}
+            </p>
         </li>
     </ul>
 </template>
+
+<style scoped>
+
+h1 {
+    text-align: center;
+}
+
+ul {
+    list-style: none;
+    padding: 0;
+    max-width: 700px;
+    margin: 0 auto;
+}
+
+li {
+    border: 1px solid #ddd;
+    border-radius: 12px;
+    padding: 20px;
+    margin-bottom: 20px;
+}
+
+</style>
