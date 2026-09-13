b7ca4824646de5a642c426daea222afcc64e29d7
Fiona Friberg <fionafriberg@outlook.com>
2026-09-13 14:56:33 +0200

    add links to navbar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
+  <nav>
+    <RouterLink to="/addTodo">
+      Add new
+    </RouterLink>
+
+    <RouterLink to="/">
+      Let's go!
+    </RouterLink>
+
+    <RouterLink to="/myTodos">
+      Todo's List
+    </RouterLink>
+  </nav>
+
   <RouterView />
 </template>
